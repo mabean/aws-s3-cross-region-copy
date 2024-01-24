ee2fd49bd73737eec577b862f55cd83e1bdde2c7
Max Bespalov <bespalov.maxim.a@gmail.com>
2024-01-24 16:14:00 +0100

    Fix import

--- src/S3CopyCrossRegionPerformer.groovy
+++ src/S3CopyCrossRegionPerformer.groovy
@@ -5,10 +5,10 @@ import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.apigateway.model.EndpointConfiguration;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.s3.model.CopyObjectRequest;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
