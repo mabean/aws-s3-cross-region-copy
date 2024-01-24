55c43a55ce99f9b13a0204c401f4eb553eb63668
Max Bespalov <bespalov.maxim.a@gmail.com>
2024-01-24 14:25:20 +0100

    Fix mistakes

--- vars/s3CopyCrossRegion.groovy
+++ vars/s3CopyCrossRegion.groovy
@@ -20,46 +20,45 @@ def call(sourceBucket, sourceRegion, sourceFile, targetBucket, targetRegion, tar
     AmazonS3 s3ClientBuilder = null;
     AmazonS3 s3desClientBuilder = null;
     TransferManager transferManager = null;
-    try {
-        ClientConfiguration clientCfg = new ClientConfiguration();
-        clientCfg.setProtocol(Protocol.HTTPS);
-        clientCfg.setSignerOverride("S3SignerType");
-        AWSStaticCredentialsProvider credentialProvidor = new AWSStaticCredentialsProvider(
-                new BasicAWSCredentials(accessKey, secretKey));
 
-        s3ClientBuilder = AmazonS3ClientBuilder//
-                .standard()//
+    ClientConfiguration clientCfg = new ClientConfiguration();
+    clientCfg.setProtocol(Protocol.HTTPS);
+    clientCfg.setSignerOverride("S3SignerType");
+    AWSStaticCredentialsProvider credentialProvidor = new AWSStaticCredentialsProvider(
+        new BasicAWSCredentials(accessKey, secretKey));
+
+     s3ClientBuilder = AmazonS3ClientBuilder//
+             .standard()//
+             .withCredentials(credentialProvidor)//
+             .withEndpointConfiguration(new EndpointConfiguration(sourceBucket, sourceRegion))//
+             .withClientConfiguration(clientCfg)//
+             .build();
+     ListObjectsRequest lor = new ListObjectsRequest()
+             .withBucketName(sourceBucket)
+             .withPrefix(sourceFile);
+     ObjectListing objectListing = s3ClientBuilder.listObjects(lor);
+     for (S3ObjectSummary summary: objectListing.getObjectSummaries()) {
+
+        SOURCE_KEY=summary.getKey();
+        DESTINATION_KEY=SOURCE_KEY
+
+        s3desClientBuilder = AmazonS3ClientBuilder//
+            .standard()//
                 .withCredentials(credentialProvidor)//
-                .withEndpointConfiguration(new EndpointConfiguration(sourceBucket, sourceRegion))//
+                .withEndpointConfiguration(new EndpointConfiguration(targetBucket, targetRegion))//
                 .withClientConfiguration(clientCfg)//
                 .build();
-        ListObjectsRequest lor = new ListObjectsRequest()
-                .withBucketName(sourceBucket)
-                .withPrefix(sourceFile);
-        ObjectListing objectListing = s3ClientBuilder.listObjects(lor);
-        for (S3ObjectSummary summary: objectListing.getObjectSummaries()) {
 
-            SOURCE_KEY=summary.getKey();
-            DESTINATION_KEY=SOURCE_KEY
+        transferManager = TransferManagerBuilder.standard()
+                .withS3Client(s3desClientBuilder)
+                .build();
 
-            s3desClientBuilder = AmazonS3ClientBuilder//
-                    .standard()//
-                    .withCredentials(credentialProvidor)//
-                    .withEndpointConfiguration(new EndpointConfiguration(targetBucket, targetRegion))//
-                    .withClientConfiguration(clientCfg)//
-                    .build();
-
-             transferManager = TransferManagerBuilder.standard()
-                    .withS3Client(s3desClientBuilder)
-                    .build();
-
-            Copy copy = transferManager.copy(new CopyObjectRequest(sourceBucket, SOURCE_KEY,
-                    targetBucket, DESTINATION_KEY),
-                    s3ClientBuilder, null);
-            copy.waitForCopyResult();   
-        }
-        transferManager.shutdownNow();
-        s3ClientBuilder.shutdown();
-        s3desClientBuilder.shutdown();
-    }
+        Copy copy = transferManager.copy(new CopyObjectRequest(sourceBucket, SOURCE_KEY,
+                targetBucket, DESTINATION_KEY),
+                s3ClientBuilder, null);
+        copy.waitForCopyResult();   
+     }
+     transferManager.shutdownNow();
+     s3ClientBuilder.shutdown();
+     s3desClientBuilder.shutdown();
 }
